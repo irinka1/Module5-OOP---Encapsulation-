8e9e24224a52602aa1d65119d33cd1cbbcb6c508
Ирина <irenmast@gmail.com>
2017-07-27 23:58:27 +0300

    Update OOP.java
    4

--- OOP.java
+++ OOP.java
@@ -10,12 +10,8 @@ public class OOP {
         CarWheel wheel1 = new CarWheel();
         Car car1 = new Car("12/05/2017","electric", 100, 15, 8, 1,50);
 
-        wheel1.steretWheel(5);
+        wheel1.steretWheel(15);
         wheel1.infoWheel();
-
-        car1.PassangersPlus();
-        car1.increseWheel(5);
-        car1.maxSpeed(5);
         car1.infoCar();
 
 
