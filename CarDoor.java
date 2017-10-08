47e94f83fd335ef278f54a6ea308698affca9c49
Ирина <irenmast@gmail.com>
2017-10-08 17:04:19 +0300

    Update CarDoor.java

--- CarDoor.java
+++ CarDoor.java
@@ -4,49 +4,49 @@ package DZ5;
 
 
 public class CarDoor {
-    private boolean door;   ///начальное положение двери - закрыта
+    private boolean door;   ///the initial position of the door is closed
     private boolean window;
 
     public CarDoor() {
-        this.door = true;   ///начальное положение окна - закрыто
+        this.door = true;   ///the initial position of the window is closed
         this.window = true;
 
     }
 
-    public void openDoor() {    //открыть дверь
+    public void openDoor() {    /
         if (door) {
-            System.out.println("Дверь открыли");
+            System.out.println("The door was opened");
             door = false;
         }
     }
 
 
-    public void closeDoor() {//закрыть дверь
+    public void closeDoor() {
         if (door = false) {
-            System.out.println("Дверь закрыли");
+            System.out.println("The door was closed");
             door = true;
 
         }
     }
 
 
-    public void checkDoorAndAction() {    //открыть/закрыть дверь
-        //(если дверь открыта и вызывается эта функция, значит дверь необходимо закрыть и наоборот)
+    public void checkDoorAndAction() {  
+       
         if (door) {
-            System.out.println("Открыть закрытую дверь");
+            System.out.println("Open the closed door");
             door = false;
 
         } else 
         {
 
-            System.out.printf("Закрыть открытую дверь");
+            System.out.printf("Close the open door");
             door = true;
         }
     }
 
     public void openWindow() {
         if (window) {
-            System.out.println("Открыть окно");
+            System.out.println("Open the window");
             window=false;
 
         }
@@ -54,38 +54,37 @@ public class CarDoor {
 
     public void closeWindow() {
         if (window = false) {
-            System.out.println("Закрыть окно");
+            System.out.println("Close a window");
 
         }
 
     }
-
-    //открыть/закрыть окно(если дверь открыта и вызывается эта функция, значит дверь необходимо закрыть и наоборот)
+// open / close the window (if the door is open and this function is called, then the door must be closed and vice versa)
     public void checkWindowAndAction() {
         if (window = true) {
-            System.out.println("необходимо открыть закрытое окно");
+            System.out.println("you need to open a closed window");
             window = false;
         } else {
-            System.out.println("Необходимо зарыть открытое окно");
+            System.out.println("It is necessary to bury the open window");
             window = true;
 
         }
     }
 
-    //Вывести в консоль данные об объекте
+    //Display information about the object in the console
     public void infoDoor() {
         if (door = true) {
-            System.out.println("Дверь закрыта");
+            System.out.println("The door is closed");
         } else {
-            System.out.println("Дверь открыта");
+            System.out.println("The door is open");
         }
     }
 
     public void infoWindow() {
         if (window = true) {
-            System.out.println("Окно закрыто");
+            System.out.println("Closed window");
         } else {
-            System.out.println("Окно открыто");
+            System.out.println("The window is open");
         }
     }
 }
